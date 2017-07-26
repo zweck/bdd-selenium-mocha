e67ae00f01048e7640f3f5afef3ff9939ec22fc2
Phil Hauser <phil@zweck.co>
2017-07-26 13:11:14 +0100

    Use xpath to target buttons by text

--- PageObjects/PlanConfiguration.js
+++ PageObjects/PlanConfiguration.js
@@ -1,4 +1,4 @@
-const { By, until } = require('selenium-webdriver');
+const { By, until, Condition } = require('selenium-webdriver');
 const { DEFAULT_WAIT_TIME } = require('../config');
 const error = require('../lib/error');
 
@@ -7,8 +7,6 @@ const element ='section.plan-configuration';
 const name = 'PlanConfiguration'
 
 // elements
-const NEW_BLANK_DRAFT_BUTTON = 'h3 .new-draft';
-const NEW_DRAFT_WITH_CONFIG_BUTTON = 'h3 .new-draft-config';
 const EMPTY_YEAR_SELECT = '.financial-year-select .Select-placeholder';
 const EMPTY_CONTRACT_BASED = '.contract-basis-select .Select-placeholder';
 const EMPTY_ORG_LEVEL_0 = '.org-level-0-select .Select-placeholder';
@@ -19,31 +17,29 @@ const CLEAR_CONFIG_BUTTON = 'button.clear-config';
 
 // Actions
 async function clickNewBlankDraft(driver){
-  const newBlankDraftWithConfigButton = await driver.wait(until.elementLocated(By.css(NEW_DRAFT_WITH_CONFIG_BUTTON)), DEFAULT_WAIT_TIME, 'No new draft with config button found');
-  const newBlankDraftButton = await driver.wait(until.elementLocated(By.css(NEW_BLANK_DRAFT_BUTTON)), DEFAULT_WAIT_TIME, 'No new draft button found');
+  const newBlankDraftWithConfigButton = await driver.wait(until.elementLocated(By.xpath("//*[contains(text(), 'New Draft With Config')]")), DEFAULT_WAIT_TIME, 'No new draft with config button found');
+  const newBlankDraftButton = await driver.wait(until.elementLocated(By.xpath("//*[contains(text(), 'New Blank Draft')]")), DEFAULT_WAIT_TIME, 'No new draft button found');
   const expandConfig = await driver.wait(until.elementLocated(By.css(EXPAND_CONFIG)), DEFAULT_WAIT_TIME, 'No expand config found');
   expandConfig.click();
   newBlankDraftButton.click();
 }
 
 async function configIsEmpty(driver){
-
-  let emptyYearSelect = await driver.wait(until.elementLocated(By.css(EMPTY_YEAR_SELECT)), DEFAULT_WAIT_TIME, `Couldn't find empty year select`);
-  let contractBasedSelect = await driver.wait(until.elementLocated(By.css(EMPTY_CONTRACT_BASED)), DEFAULT_WAIT_TIME, `Couldn't find contract based select`);
-  let emptyOrgLevel0 = await driver.wait(until.elementLocated(By.css(EMPTY_ORG_LEVEL_0)), DEFAULT_WAIT_TIME, `Couldn't find empty org level 0 select`);
-  let emptyUnit = await driver.wait(until.elementLocated(By.css(EMPTY_UNIT)), DEFAULT_WAIT_TIME, `Couldn't find empty unti select`);
-  let planName = await driver.wait(until.elementLocated(By.css(PLAN_NAME)), DEFAULT_WAIT_TIME, `Couldn't find empty plan name textbox`);
+  const emptyYearSelect = await driver.wait(until.elementLocated(By.css(EMPTY_YEAR_SELECT)), DEFAULT_WAIT_TIME, `Couldn't find empty year select`);
+  const contractBasedSelect = await driver.wait(until.elementLocated(By.css(EMPTY_CONTRACT_BASED)), DEFAULT_WAIT_TIME, `Couldn't find contract based select`);
+  const emptyOrgLevel0 = await driver.wait(until.elementLocated(By.css(EMPTY_ORG_LEVEL_0)), DEFAULT_WAIT_TIME, `Couldn't find empty org level 0 select`);
+  const emptyUnit = await driver.wait(until.elementLocated(By.css(EMPTY_UNIT)), DEFAULT_WAIT_TIME, `Couldn't find empty unti select`);
+  const planName = await driver.wait(until.elementLocated(By.css(PLAN_NAME)), DEFAULT_WAIT_TIME, `Couldn't find empty plan name textbox`);
 
   if( planName.getText().length ) {
     return error({ driver, message: `plan name isn't empty` });
   } else {
     return true;
   }
-
 }
 
 async function clearConfigButtonExists(driver){
-  let clearConfigButton = await driver.wait(until.elementLocated(By.css(CLEAR_CONFIG_BUTTON)), DEFAULT_WAIT_TIME, `Couldn't find clear config button`);
+  const clearConfigButton = await driver.wait(until.elementLocated(By.xpath("//*[contains(text(), 'Clear Config')]")), DEFAULT_WAIT_TIME, `Couldn't find clear config button`);
   return true;
 }
 
