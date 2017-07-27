f9282a3a3a1ea36a869f836207d6eaeb342383e9
Howie Weiner <hello@builtbyhowie.co.uk>
2017-07-27 16:19:47 +0100

    screenshots added for failed tests

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,23 +1,27 @@
 'use strict';
 const assert = require('assert');
 const { Builder, By, promise, until } = require('selenium-webdriver');
+const { screenshot } = require('../lib/utils.js');
+
 promise.USE_PROMISE_MANAGER = false;
 
 describe( 'Test runner', () => {
 
   let driver;
 
-  // We use this to tear down chrome 
-  // at the end of every test
-  beforeEach(async () => {
+  before(async () => {
     driver = await new Builder()
       .forBrowser( 'chrome' )
       .build();
     driver.manage().window().setSize(1280, 720)
   });
 
-  afterEach(() => {
+  after(() => {
     driver.quit();
+  });
+
+  afterEach(async function() {
+    await screenshot(this.currentTest, driver);
   });
 
   describe( 'example test', () => {
